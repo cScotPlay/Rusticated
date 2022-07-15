552fbadeeb9de9d29e675e4cb3a5e313d3b10a78
cScot <cscot@nmd.so>
2022-07-15 15:16:57 -0700

    added path blocks

---
+++ src/main/java/net/mcs3/elixiremporium/world/level/block/PathBlock.java
@@ -0,0 +1,31 @@
+package net.mcs3.elixiremporium.world.level.block;
+
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
+
+public class PathBlock extends Block
+{
+    private static final VoxelShape SHAPE = Block.box(1, 0, 1, 15, 1, 15);
+
+    public PathBlock() {
+        super(BlockBehaviour.Properties.copy(Blocks.STONE));
+    }
+
+    @Override
+    public VoxelShape getShape (BlockState state, BlockGetter level, BlockPos blockPos, CollisionContext context)
+    {
+        return SHAPE;
+    }
+
+    @Override
+    public VoxelShape getOcclusionShape(BlockState state, BlockGetter level, BlockPos pos) {
+        return SHAPE;
+    }
+}
