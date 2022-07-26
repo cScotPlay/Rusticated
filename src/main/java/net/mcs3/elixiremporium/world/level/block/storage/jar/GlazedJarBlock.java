36a71c480bf1e64d2ae7f660d91f47ef8e25fcf6
cScot <cscotplay@gmail.com>
2022-07-26 13:27:13 -0700

    updated voxel shapes for jar tops

--- src/main/java/net/mcs3/elixiremporium/world/level/block/storage/jar/GlazedJarBlock.java
+++ src/main/java/net/mcs3/elixiremporium/world/level/block/storage/jar/GlazedJarBlock.java
@@ -67,7 +67,7 @@ public class GlazedJarBlock extends AbstractStorageBlock implements EntityBlock
         JAR_SHAPE_1 = Shapes.or(box(3.0, 0.0, 3.0, 13.0, 2.0, 13.0), box(2.0, 2.0, 2.0, 14.0, 4.0, 14.0), box(1.0, 4.0, 1.0, 15.0, 8.0, 15.0));
         JAR_SHAPE_2 = Shapes.or(box(2.0, 8.0, 2.0, 14.0, 10.0, 14.0),box(3.0, 10.0, 3.0, 13.0, 11.0, 12.0), box(4.0, 11.0, 4.0, 12.0, 12.0, 12.0));
         JAR_SHAPE_3 = Shapes.or(box(5.0, 12.0, 5.0, 11.0, 13.0, 11.0),box(6.0, 13.0, 6.0, 10.0, 14.0, 10.0), box(5.0, 14.0, 5.0, 11.0, 15.0, 11.0));
-        JAR_SHAPE_4 = Shapes.box(4.0, 15.0, 4.0, 12.0, 16.0, 12.0);
+        JAR_SHAPE_4 = Shapes.or(box(4.0, 15.0, 4.0, 12.0, 16.0, 12.0));
         JAR_AABB = Shapes.or(JAR_SHAPE_1, JAR_SHAPE_2, JAR_SHAPE_3, JAR_SHAPE_4);
     }
 }
