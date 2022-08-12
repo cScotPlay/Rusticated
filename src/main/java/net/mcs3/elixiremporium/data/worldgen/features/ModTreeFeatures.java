8e628a7027df7072c12b137ba6c8885f40a44865
cScot <cscotplay@gmail.com>
2022-08-11 22:31:28 -0700

    added ironwood tree features

--- src/main/java/net/mcs3/elixiremporium/data/worldgen/features/ModTreeFeatures.java
+++ src/main/java/net/mcs3/elixiremporium/data/worldgen/features/ModTreeFeatures.java
@@ -4,12 +4,11 @@ import net.mcs3.elixiremporium.init.ModBlocks;
 import net.minecraft.core.Holder;
 import net.minecraft.data.worldgen.features.FeatureUtils;
 import net.minecraft.util.valueproviders.ConstantInt;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.configurations.TreeConfiguration;
 import net.minecraft.world.level.levelgen.feature.featuresize.TwoLayersFeatureSize;
-import net.minecraft.world.level.levelgen.feature.foliageplacers.AcaciaFoliagePlacer;
+import net.minecraft.world.level.levelgen.feature.foliageplacers.RandomSpreadFoliagePlacer;
 import net.minecraft.world.level.levelgen.feature.stateproviders.BlockStateProvider;
 import net.minecraft.world.level.levelgen.feature.trunkplacers.ForkingTrunkPlacer;
 
@@ -19,10 +18,12 @@ public class ModTreeFeatures
 
     public static void init()
     {
-        IRONWOOD_TREE_FEATURE = FeatureUtils.register("ironwood_tree_feature", Feature.TREE, new TreeConfiguration.TreeConfigurationBuilder(BlockStateProvider.simple(Blocks.ACACIA_LOG),
-                new ForkingTrunkPlacer(5, 2, 2), BlockStateProvider.simple(ModBlocks.IRONWOOD_LEAVES),
-                new AcaciaFoliagePlacer(ConstantInt.of(2), ConstantInt.of(0)),
-                new TwoLayersFeatureSize(1, 0, 2)).ignoreVines().build());
+        IRONWOOD_TREE_FEATURE = FeatureUtils.register("ironwood_tree_feature", Feature.TREE, new TreeConfiguration.TreeConfigurationBuilder(
+                BlockStateProvider.simple(ModBlocks.IRONWOOD_LOG),
+                new ForkingTrunkPlacer(4, 3, 4),
+                BlockStateProvider.simple(ModBlocks.IRONWOOD_LEAVES),
+                new RandomSpreadFoliagePlacer(ConstantInt.of(3), ConstantInt.of(0), ConstantInt.of(2), 100),
+                new TwoLayersFeatureSize(2, 0, 2)).ignoreVines().build());
     }
 }
 
