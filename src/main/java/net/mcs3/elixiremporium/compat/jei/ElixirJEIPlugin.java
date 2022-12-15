90a5aec809911d44bfedf53ab500b0dc6de1b95f
cScot <cscotplay@gmail.com>
2022-12-15 15:24:14 -0800

    added JEI integration for adv condenser

--- src/main/java/net/mcs3/elixiremporium/compat/jei/ElixirJEIPlugin.java
+++ src/main/java/net/mcs3/elixiremporium/compat/jei/ElixirJEIPlugin.java
@@ -4,9 +4,11 @@ import mezz.jei.api.IModPlugin;
 import mezz.jei.api.helpers.IGuiHelper;
 import mezz.jei.api.registration.*;
 import net.mcs3.elixiremporium.ElixirEmporium;
+import net.mcs3.elixiremporium.client.screens.inventory.AdvCondenserScreen;
 import net.mcs3.elixiremporium.client.screens.inventory.CondenserScreen;
 import net.mcs3.elixiremporium.init.ModBlocks;
 import net.mcs3.elixiremporium.init.ModItems;
+import net.mcs3.elixiremporium.world.item.crafting.AdvCondenserRecipe;
 import net.mcs3.elixiremporium.world.item.crafting.CondenserRecipe;
 import net.minecraft.client.Minecraft;
 import net.minecraft.resources.ResourceLocation;
@@ -28,15 +30,19 @@ public class ElixirJEIPlugin implements IModPlugin {
 
         IGuiHelper guiHelper = registration.getJeiHelpers().getGuiHelper();
         registration.addRecipeCategories(
-                new CondenserRecipeCategory(guiHelper)
+                new CondenserRecipeCategory(guiHelper),
+                new AdvCondenserRecipeCategory(guiHelper)
         );
     }
 
     @Override
     public void registerRecipes(IRecipeRegistration registration) {
         RecipeManager rm = Objects.requireNonNull(Minecraft.getInstance().level).getRecipeManager();
-        List<CondenserRecipe> recipes = rm.getAllRecipesFor(CondenserRecipe.Type.INSTANCE);
-        registration.addRecipes(CondenserRecipeCategory.TYPE, recipes);
+        List<CondenserRecipe> condenserRecipes = rm.getAllRecipesFor(CondenserRecipe.Type.INSTANCE);
+        registration.addRecipes(CondenserRecipeCategory.TYPE, condenserRecipes);
+
+        List<AdvCondenserRecipe> advCondenserRecipes = rm.getAllRecipesFor(AdvCondenserRecipe.Type.INSTANCE);
+        registration.addRecipes(AdvCondenserRecipeCategory.TYPE, advCondenserRecipes);
     }
 
 
@@ -44,11 +50,13 @@ public class ElixirJEIPlugin implements IModPlugin {
     @SuppressWarnings("Deprecated")
     public void registerRecipeCatalysts(IRecipeCatalystRegistration registration) {
         registration.addRecipeCatalyst(new ItemStack(ModBlocks.CONDENSER), CondenserRecipeCategory.UID);
+        registration.addRecipeCatalyst(new ItemStack(ModBlocks.ADV_CONDENSER), AdvCondenserRecipeCategory.UID);
     }
 
     @Override
     public void registerGuiHandlers(IGuiHandlerRegistration registration) {
         registration.addRecipeClickArea(CondenserScreen.class, 44, 29, 50, 28, CondenserRecipeCategory.TYPE);
+        registration.addRecipeClickArea(AdvCondenserScreen.class, 44, 29, 50, 28, AdvCondenserRecipeCategory.TYPE);
     }
 
     @Override
