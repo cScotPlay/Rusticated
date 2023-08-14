038664bf53e146e9d685ab1ebe8681c68abaddbb
Anonymous <Sunconure11@users.noreply.github.com>
2023-08-13 20:44:49 -0400

    Test with this

--- src/main/java/net/mcs3/rusticated/world/level/levelgen/generation/VegetationGeneration.java
+++ src/main/java/net/mcs3/rusticated/world/level/levelgen/generation/VegetationGeneration.java
@@ -25,8 +25,7 @@ public class VegetationGeneration
         BiomeModifications.addFeature(BiomeSelectors.tag(BiomeTags.IS_SAVANNA), GenerationStep.Decoration.VEGETAL_DECORATION, ModVegetationPlacement.ALOE_VERA_DESERT.unwrapKey().get());
         BiomeModifications.addFeature(BiomeSelectors.tag(BiomeTags.IS_JUNGLE), GenerationStep.Decoration.VEGETAL_DECORATION, ModVegetationPlacement.JUNGLE_HERBS.unwrapKey().get());
         BiomeModifications.addFeature(BiomeSelectors.includeByKey(Biomes.PLAINS), GenerationStep.Decoration.VEGETAL_DECORATION, ModVegetationPlacement.PLAINS_HERBS.unwrapKey().get());
-        BiomeModifications.addFeature(BiomeSelectors.tag(BiomeTags.IS_FOREST), GenerationStep.Decoration.VEGETAL_DECORATION, ModVegetationPlacement.FOREST_HERBS.unwrapKey().get());
-        BiomeModifications.addFeature(BiomeSelectors.tag(BiomeTags.IS_TAIGA), GenerationStep.Decoration.VEGETAL_DECORATION, ModVegetationPlacement.FOREST_HERBS.unwrapKey().get());
+        BiomeModifications.addFeature(BiomeSelectors.tag(BiomeTags.IS_FOREST).or(BiomeSelectors.tag(BiomeTags.IS_TAIGA)), GenerationStep.Decoration.VEGETAL_DECORATION, ModVegetationPlacement.FOREST_HERBS.unwrapKey().get());
         BiomeModifications.addFeature(BiomeSelectors.includeByKey(Biomes.SWAMP), GenerationStep.Decoration.VEGETAL_DECORATION, ModVegetationPlacement.SWAMP_HERBS.unwrapKey().get());
         BiomeModifications.addFeature(BiomeSelectors.includeByKey(Biomes.MANGROVE_SWAMP), GenerationStep.Decoration.VEGETAL_DECORATION, ModVegetationPlacement.SWAMP_HERBS.unwrapKey().get());
         BiomeModifications.addFeature(BiomeSelectors.tag(BiomeTags.IS_MOUNTAIN), GenerationStep.Decoration.VEGETAL_DECORATION, ModVegetationPlacement.MOUNTAINOUS_HERBS.unwrapKey().get());
